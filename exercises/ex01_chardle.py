2e93758825606d4f11a84d18e0b3325da18233d5
Luke Zakia-Fahey <lzak@unc.edu>
2023-09-08 00:40:54 -0400

    ex01 backup

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,49 +2,49 @@
 __author__ = "730679428"
 
 #ask user for the word
-word: str = input("Enter a 5-character word: ")
+user_inputted_word: str = input("Enter a 5-character word: ")
 #throw an error if the user-inputted word is not 5 character
-if len(word)!=5:
+if len(user_inputted_word)!=5:
     print("Error: Word must contain 5 characters")
     exit()
 #ask user for letter
-letter: str = input("Enter a single character: ")
+user_inputted_letter: str = input("Enter a single character: ")
 #throw an error if user-inputted letter is more than one character
-if len(letter)!=1:
+if len(user_inputted_letter)!=1:
     print("Error: Character must be a single character.")
     exit()
     
-print("Searching for " + letter + " in " + word)
+print("Searching for " + user_inputted_letter + " in " + user_inputted_word)
 
 #initialize count
 count=0
 #check first character of word
-if word[0]==letter:
+if user_inputted_word[0]==user_inputted_letter:
     count+=1
-    print(letter + " found at index " + str(0))
+    print(user_inputted_letter + " found at index " + str(0))
 #check second character of word
-if word[1]==letter:
+if user_inputted_word[1]==user_inputted_letter:
     count+=1
-    print(letter + " found at index " + str(1))
+    print(user_inputted_letter + " found at index " + str(1))
 #check third character of word
-if word[2]==letter:
+if user_inputted_word[2]==user_inputted_letter:
     count+=1
-    print(letter + " found at index " + str(2))
+    print(user_inputted_letter + " found at index " + str(2))
 #check fourth character of word
-if word[3]==letter:
+if user_inputted_word[3]==user_inputted_letter:
     count+=1
-    print(letter + " found at index " + str(3))
+    print(user_inputted_letter + " found at index " + str(3))
 #check fifth character of word
-if word[4]==letter:
+if user_inputted_word[4]==user_inputted_letter:
     count+=1
-    print(letter + " found at index " + str(4))
+    print(user_inputted_letter + " found at index " + str(4))
 
 #print how many instances of letter were found in word
 if count==0:
-    print("No instances of " + letter + " found in " + word)
+    print("No instances of " + user_inputted_letter + " found in " + user_inputted_word)
 if count==1:
-    print("1 instance of " + letter + " found in " + word)
+    print("1 instance of " + user_inputted_letter + " found in " + user_inputted_word)
 if count==2:
-    print("2 instances of " + letter + " found in " + word)
+    print("2 instances of " + user_inputted_letter + " found in " + user_inputted_word)
 
 
